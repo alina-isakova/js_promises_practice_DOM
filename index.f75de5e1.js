4dc032333c89141b6727c723d3fa0160abcfcb73
Alina Isakova <adorosh062@gmail.com>
2025-03-06 13:55:50 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,34 @@
+"use strict";
+const firstPromise = new Promise((resolve, reject)=>{
+    document.body.addEventListener("click", ()=>{
+        resolve("First promise was resolved");
+    });
+    setTimeout(()=>{
+        reject(new Error("First promise was rejected"));
+    }, 3000);
+});
+firstPromise.then((result)=>addSuccessMessage(result)).catch((error)=>addErrorMessage(error));
+const secondPromise = new Promise((resolve)=>{
+    document.body.addEventListener("mousedown", ()=>{
+        resolve("Second promise was resolved");
+    });
+});
+secondPromise.then((result)=>addSuccessMessage(result));
+const thirdPromise = new Promise((resolve)=>{
+    let leftClicked = false;
+    let rightClicked = false;
+    document.body.addEventListener("mousedown", (e)=>{
+        if (e.button === 0) leftClicked = true;
+        if (e.button === 2) rightClicked = true;
+        if (leftClicked && rightClicked) resolve("Third promise was resolved");
+    });
+});
+thirdPromise.then((result)=>addSuccessMessage(result));
+function addSuccessMessage(message) {
+    document.body.insertAdjacentHTML("beforeend", `<div class="success" data-qa="notification">${message}</div>`);
+}
+function addErrorMessage(message) {
+    document.body.insertAdjacentHTML("beforeend", `<div class="error" data-qa="notification">${message}</div>`);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
